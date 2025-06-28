465098c5fdc1fc3cdfabd4658c64c6793875169a
SamsaniMonish <samsanimonish@gmail.com>
2025-06-28 17:24:27 +0200

    second commit

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -111,7 +111,7 @@ const ProfilePage = (props) => {
         <div className="flexbox info">
           <img src="/avatar.jpg" alt="Avatar" className="avatar" />
           <div className="flexbox text">
-            <h1>Swasthi</h1>
+            <h1>iGebra.AI</h1>
             <h3>
               Ongoing Courses: <b>{Object.keys(topics).length}</b>
             </h3>
